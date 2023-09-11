20abf6b0bc458ccfc9119173226a3b632eb8c50b
rhodinemma <rhodinemma10@gmail.com>
2023-09-11 16:42:58 +0300

    feat: marshaling data struct to json representation

--- main.go
+++ main.go
@@ -1,14 +1,22 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
 )
 
+type Customer struct {
+	Name    string `json:"full_name"`
+	City    string `json:"city"`
+	Zipcode string `json:"zip_code"`
+}
+
 func main() {
 	// define routes
 	http.HandleFunc("/greet", greet)
+	http.HandleFunc("/customers", getAllCustomers)
 
 	// starting the server
 	log.Fatalln(http.ListenAndServe("localhost:8000", nil))
@@ -16,6 +24,22 @@ func main() {
 
 func greet(w http.ResponseWriter, r *http.Request) {
 	_, err := fmt.Fprintf(w, "Hello Rhodin GO......")
+	if err != nil {
+		return
+	}
+}
+
+func getAllCustomers(w http.ResponseWriter, r *http.Request) {
+	customers := []Customer{
+		{"Rhodin", "Mukono", "110075"},
+		{"Emmanuel", "Kampala", "110075"},
+		{"Mariam", "Seeta", "110075"},
+	}
+
+	// set response content-type header
+	w.Header().Add("Content-Type", "application/json")
+
+	err := json.NewEncoder(w).Encode(customers)
 	if err != nil {
 		return
 	}
